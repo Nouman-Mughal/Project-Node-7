e25c3e8697dc2f36c47d280e261b96f56a7281e3
Noman-Ahmad <programmer.noman786@gmail.com>
2022-10-12 17:05:57 +0500

    added presentation

--- routes/index.mjs
+++ routes/index.mjs
@@ -4,7 +4,7 @@ const router=express.Router()
 //@description Login/landing page
 //@ actual route GET /
 router.get('/',(req,res)=>{
-    res.render('login')
+    res.render('login',{layout:'login'})
 })
 //@description /dashboard
 //@ actual route GET /dashboard
